dd0e2a622498f611954ed469cb97cd0559ce70fc
Devon Tubben <tubbend1@msu.edu>
2021-07-13 13:49:01 -0400

    moved nav bar into components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Components/Home' 
-import NavBar from './NavBar'
+import NavBar from './Components/NavBar'
 import Weather from './Containers/Weather'
 
 function App() {
--- src/Containers/Weather.js
+++ src/Containers/Weather.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react' 
 
 class Weather extends Component{ 
+    constructor(){ 
+        super()
+        this.state = { 
+            weather: []
+        }
+    }
+
+    componentDidMount(){ 
+        fetch(`http://localhost:3001/Weather`)
+        .then(res => res.json()) 
+        .then(weather => this.setState({ weather }))
+    }
+
     render(){ 
         return( 
             <div> 
