714368b5fe85f9401c0fe7d3f5a2f02532a65798
Devon Tubben <tubbend1@msu.edu>
2021-07-13 13:28:18 -0400

    updated navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react' 
+import { NavLink } from 'react-router-dom'
+
+const link = { 
+    width: '100px',
+    padding: '12px',
+    margin: '0 6px 6px',
+    background: 'black',
+    textDecoration: 'none',
+    color: 'white'
+
+}
 
 class NavBar extends Component{ 
+
     render(){ 
-        return(<nav> Nav Bar </nav>)
+        return(
+            <div> 
+                <NavLink
+                to='/' 
+                exact
+                Home 
+                ></NavLink>
+               
+            </div>
+        )
     }
 }
 
-export default NavBar
+export default NavBar;
