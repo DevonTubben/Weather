905585c3177f9a11e607893ec5b9fe73761fde64
Devon Tubben <tubbend1@msu.edu>
2021-07-13 12:36:02 -0400

    localhost is working properly

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react' 
+
+class Home extends Component { 
+    render(){ 
+        return( 
+            <div> 
+                <h1> 
+                    Weather App
+                </h1>
+                <p> 
+                    Check the Weather in your area:
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Home;
