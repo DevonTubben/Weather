423197f30f6fd8b42a2ee463238d9f121bf17e51
Devon Tubben <tubbend1@msu.edu>
2021-07-13 16:46:47 -0400

    first days work

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react' 
 
 class Home extends Component { 
+
+//  const (form, setForm) = useState({
+//      city: " ",
+//      country: " "
+//  })
+
+const handleChange = (e) => { 
+    let name = e.target.name 
+    let value = e.target.value
+
+    if(name == "city"){ 
+        setForm({...form, city:value})
+    }
+    if(name == "country"){ 
+        setForm({...form, country:value})
+    }
+    console.log(city, country)
+}
+
+
+
     render(){ 
         return( 
             <div> 
@@ -11,7 +32,7 @@ class Home extends Component {
                     Check the Weather in your area:
                 </p>
                 <form> 
-                    <input type="text" name="city" placeholder="city"/>
+                    <input type="text" name="city" placeholder="city" onChange={e => handleChange(e)}  />
                     <input type="text" name="country" placeholder="country"/>
                     <button className="getweather">Submit</button>
                 </form>
