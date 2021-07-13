31e5b0798e125c7d26fbe2a619086f3c8c37e089
Devon Tubben <tubbend1@msu.edu>
2021-07-13 12:45:32 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './Components/Home' 
+import NavBar from './NavBar'
 
 function App() {
   return (
+    <Router> 
     <div className="App">
+      <NavBar />
+      <Switch> 
+        <Route exact path='/' component={Home}/>
+      </Switch>
+
       <Home />
     </div>
+    </Router>
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react' 
+
+class NavBar extends Component{ 
+    render(){ 
+        return(<nav> Nav Bar </nav>)
+    }
+}
+
+export default NavBar
