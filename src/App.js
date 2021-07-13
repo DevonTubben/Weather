c752f53e953a0e7c82e708b27489e193945de646
Devon Tubben <tubbend1@msu.edu>
2021-07-13 13:39:28 -0400

    created a weather file and added it to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './Components/Home' 
 import NavBar from './NavBar'
+import Weather from './Containers/Weather'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Switch> 
         <Route exact path='/' component={Home}/>
+        <Route exact path='/Weather' component={Weather}/>
       </Switch>
 
     </div>
